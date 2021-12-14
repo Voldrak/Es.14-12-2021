c8bc4fdc7533d6707f043978e496ce77337fc55e
MarcoM94 <voldrak94@gmail.com>
2021-12-14 17:17:41 +0100

    feat: inserita l'IVA

--- src/components/CardDescription/index.jsx
+++ src/components/CardDescription/index.jsx
@@ -2,11 +2,11 @@ import './styles.css'
 //import ColorDot from '../ColorDot'
 //const colorArray = ['#FF6633', '#FFB399', '#FF33FF'];
 
-const CardDescription = ({ title, price, description }) => (
+const CardDescription = ({ title, price, vat, description }) => (
   <div className="card-description">
     <div className="card-description__priceTitle">
       <p className="card-description__title">{title}</p>
-      <p className="card-description__price">{price}</p>
+      <p className="card-description__price">{price} (IVA inclusa)</p>
     </div>
     <div className="card-description__descriptionContainer">
       <p className="card-description__description">{description}</p>
--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -3,11 +3,11 @@ import './styles.css';
 import CardDescription from '../CardDescription'
 import CardImage from '../CardImage'
 
-
 const ProductCard = ({ title, price, description, image }) => (
   <div className="product-card">
     <CardImage src={image} />
-    <CardDescription title={title} description={description} price={price} />
+    <CardDescription title={title} description={description}  price={parseFloat(price+((price*22)/100)).toFixed(2)} />
+    
   </div>
 )
 
